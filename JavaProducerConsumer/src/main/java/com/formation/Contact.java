a074bfd36db49c830f283585fb69ac6eb4bb0f34
anghour <anghour@gmail.com>
2018-03-14 16:57:38 +0100

    Premier projet KAFKA JAVA

---
+++ JavaProducerConsumer/src/main/java/com/formation/Contact.java
@@ -0,0 +1,23 @@
+package com.formation;
+
+public class Contact {
+
+    private String name;
+    private  String mail;
+
+    public String getMail(){
+        return  this.mail;
+    }
+
+    public void setMail(String mail){
+        this.mail = mail;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
